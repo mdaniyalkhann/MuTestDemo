414f7019898f3726052e4245a17f7cea6ac9ac9c
mdaniyalkhann <mdaniyal.khan@aurea.com>
2020-06-26 00:14:09 +0500

    add some asserts

--- MuTestDemoTests/SchoolOfMutantsTests.cs
+++ MuTestDemoTests/SchoolOfMutantsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using MuTestDemo;
 using NUnit.Framework;
 using Shouldly;
@@ -10,24 +9,24 @@ namespace MuTestDemoTests
     [ExcludeFromCodeCoverage]
     public class SchoolOfMutantsTests
     {
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        [TestCase(4)]
-        [TestCase(9)]
-        [TestCase(16)]
-        [TestCase(24)]
-        [TestCase(81)]
-        public void TrainMutants_WhenCalled_VerifyMutants(int mutantCounts)
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 4)]
+        [TestCase(4, 4)]
+        [TestCase(9, 5)]
+        [TestCase(16, 10)]
+        [TestCase(24, 14)]
+        [TestCase(81, 82)]
+        public void TrainMutants_WhenCalled_VerifyMutants(int mutantCounts, int expected)
         {
             // Arrange
             var instance = new SchoolOfMutants(new Log());
 
             // Act
-            Action action = () => instance.TrainMutants(mutantCounts);
+            var result = instance.TrainMutants(mutantCounts);
 
             // Assert
-            action.ShouldNotThrow();
+            result.ShouldBe(expected);
         }
     }
 }
